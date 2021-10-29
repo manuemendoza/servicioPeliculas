9bfc786f9ecede8c02b5323bcc37374f6d447bd0
Manuel Mendoza <77455281+manuemendoza@users.noreply.github.com>
2021-10-29 09:27:02 +0200

    creating the base of the server part of users

--- app/users/router.js
+++ app/users/router.js
@@ -1,3 +1,11 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
-// const controller = require('./controller.js');
+const controller = require('./controller.js');
+
+router.get('/:id', controller);
+router.get('/:id', controller);
+router.post('/:id', controller);
+router.delete('/:id', controller);
+
+
+module.exports = router;
