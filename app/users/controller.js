f1f29b8ca681fbb31eb0ebef940a2817db2d2f9c
Manuel Mendoza <77455281+manuemendoza@users.noreply.github.com>
2021-10-29 09:59:36 +0200

    "creating base"

---
+++ app/users/controller.js
@@ -0,0 +1,9 @@
+// const users = require('./model.js');
+
+// const createUser;
+// const validationUser;
+
+// module.exports = {
+//     createUser,
+//     validationUser
+// };
--- app/users/router.js
+++ app/users/router.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-const controller = require('./controller.js');
+// const controller = require('./controller.js');
 
-router.get('/:id', controller);
-router.get('/:id', controller);
-router.post('/:id', controller);
-router.delete('/:id', controller);
+// router.get('/:id', controller);
+// router.get('/', controller);
+// router.post('/:id', controller.validationUser);
+// router.post('/', controller.createUser);
+// router.delete('/:id', controller);
 
 
-module.exports = router;
+// module.exports = router;
